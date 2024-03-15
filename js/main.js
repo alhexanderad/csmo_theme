ce0cc7ec91fb6ff52a022b811c6f2b8dfb17d83f
Alhexander D. Astuhuaman <alhexanderad@gmail.com>
2024-03-15 16:26:07 -0500

    se realizo los cambios de color de recommended vehicles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', function () {
+  /** Codigo de Navbar de contacto */
   const navSc = document.querySelector('.navbar');
   window.addEventListener('scroll', () => {
     if (window.scrollY >= 56) {
@@ -9,4 +10,12 @@ document.addEventListener('DOMContentLoaded', function () {
       navSc.classList.remove('navbar-scrolled');
     }
   });
+
+  /**Codigo del ID lightgallery*/
+  $(document).ready(function () {
+    $('#lightgallery').lightGallery({
+      selector: '.car-magnify-gallery a',
+    });
+  });
+
 });
