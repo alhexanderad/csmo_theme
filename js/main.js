cafabd09c5db01796768e6fd7f35fdf4ac767577
Alhexander D. Astuhuaman <alhexanderad@gmail.com>
2024-03-20 16:27:26 -0500

    Se realizo la parte de slider pero se necesita dar color

--- js/main.js
+++ js/main.js
@@ -18,4 +18,23 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  /** Slick Sliders*/
+  $('.slick-carousel').each(function () {
+    var slider = $(this);
+    $(this).slick({
+      infinite: true,
+      dots: false,
+      arrows: false,
+      centerMode: true,
+      centerPadding: '0'
+    });
+
+    $(this).closest('.slick-slider-area').find('.slick-prev').on("click", function () {
+      slider.slick('slickPrev');
+    });
+    $(this).closest('.slick-slider-area').find('.slick-next').on("click", function () {
+      slider.slick('slickNext');
+    });
+  });
+
 });
